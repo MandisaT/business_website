c9b881dd00840d57ea3e351d60c531137a74f740
MandisaT <mandisa.thomas1@gmail.com>
2016-04-21 15:43:05 -0400

    adding layout

---
+++ app.rb
@@ -0,0 +1,23 @@
+require "sinatra"
+
+get "/page1" do 
+ erb :page1
+end 
+
+get "/page2" do 
+ erb :page2
+end 
+
+
+get "/page3" do 
+ erb :page3
+end 
+
+
+get "/page4" do 
+ erb :page4
+end 
+
+get "/layout" do 
+ erb :layout
+end 
